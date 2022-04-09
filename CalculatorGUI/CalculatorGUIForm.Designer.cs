7519ace3c27021d14378e3e06ac31fada925c12f
symuriz <syed9648@gmail.com>
2022-04-08 20:12:39 -0400

    changes made

---
+++ CalculatorGUI/CalculatorGUIForm.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace CalculatorGUI
+{
+   partial class CalculatorGUIForm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose( bool disposing )
+      {
+         if ( disposing && ( components != null ) )
+         {
+            components.Dispose();
+         }
+         base.Dispose( disposing );
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.button13 = new System.Windows.Forms.Button();
+         this.button14 = new System.Windows.Forms.Button();
+         this.button15 = new System.Windows.Forms.Button();
+         this.button16 = new System.Windows.Forms.Button();
+         this.button9 = new System.Windows.Forms.Button();
+         this.button10 = new System.Windows.Forms.Button();
+         this.button11 = new System.Windows.Forms.Button();
+         this.button12 = new System.Windows.Forms.Button();
+         this.button5 = new System.Windows.Forms.Button();
+         this.button6 = new System.Windows.Forms.Button();
+         this.button7 = new System.Windows.Forms.Button();
+         this.button8 = new System.Windows.Forms.Button();
+         this.button4 = new System.Windows.Forms.Button();
+         this.button3 = new System.Windows.Forms.Button();
+         this.button2 = new System.Windows.Forms.Button();
+         this.button1 = new System.Windows.Forms.Button();
+         this.textBox1 = new System.Windows.Forms.TextBox();
+         this.SuspendLayout();
+         // 
+         // button13
+         // 
+         this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button13.Location = new System.Drawing.Point( 189, 180 );
+         this.button13.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button13.Name = "button13";
+         this.button13.Size = new System.Drawing.Size( 63, 50 );
+         this.button13.TabIndex = 33;
+         this.button13.Text = "*";
+         // 
+         // button14
+         // 
+         this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button14.Location = new System.Drawing.Point( 126, 180 );
+         this.button14.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button14.Name = "button14";
+         this.button14.Size = new System.Drawing.Size( 63, 50 );
+         this.button14.TabIndex = 32;
+         this.button14.Text = "=";
+         // 
+         // button15
+         // 
+         this.button15.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button15.Location = new System.Drawing.Point( 63, 180 );
+         this.button15.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button15.Name = "button15";
+         this.button15.Size = new System.Drawing.Size( 63, 50 );
+         this.button15.TabIndex = 31;
+         this.button15.Text = ".";
+         // 
+         // button16
+         // 
+         this.button16.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button16.Location = new System.Drawing.Point( 0, 180 );
+         this.button16.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button16.Name = "button16";
+         this.button16.Size = new System.Drawing.Size( 63, 50 );
+         this.button16.TabIndex = 30;
+         this.button16.Text = "0";
+         // 
+         // button9
+         // 
+         this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button9.Location = new System.Drawing.Point( 189, 130 );
+         this.button9.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button9.Name = "button9";
+         this.button9.Size = new System.Drawing.Size( 63, 50 );
+         this.button9.TabIndex = 29;
+         this.button9.Text = "/";
+         // 
+         // button10
+         // 
+         this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button10.Location = new System.Drawing.Point( 126, 130 );
+         this.button10.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button10.Name = "button10";
+         this.button10.Size = new System.Drawing.Size( 63, 50 );
+         this.button10.TabIndex = 28;
+         this.button10.Text = "3";
+         // 
+         // button11
+         // 
+         this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button11.Location = new System.Drawing.Point( 63, 130 );
+         this.button11.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button11.Name = "button11";
+         this.button11.Size = new System.Drawing.Size( 63, 50 );
+         this.button11.TabIndex = 27;
+         this.button11.Text = "2";
+         // 
+         // button12
+         // 
+         this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button12.Location = new System.Drawing.Point( 0, 130 );
+         this.button12.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button12.Name = "button12";
+         this.button12.Size = new System.Drawing.Size( 63, 50 );
+         this.button12.TabIndex = 26;
+         this.button12.Text = "1";
+         // 
+         // button5
+         // 
+         this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button5.Location = new System.Drawing.Point( 189, 80 );
+         this.button5.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button5.Name = "button5";
+         this.button5.Size = new System.Drawing.Size( 63, 50 );
+         this.button5.TabIndex = 25;
+         this.button5.Text = "*";
+         // 
+         // button6
+         // 
+         this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button6.Location = new System.Drawing.Point( 126, 80 );
+         this.button6.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button6.Name = "button6";
+         this.button6.Size = new System.Drawing.Size( 63, 50 );
+         this.button6.TabIndex = 24;
+         this.button6.Text = "6";
+         // 
+         // button7
+         // 
+         this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button7.Location = new System.Drawing.Point( 63, 80 );
+         this.button7.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button7.Name = "button7";
+         this.button7.Size = new System.Drawing.Size( 63, 50 );
+         this.button7.TabIndex = 23;
+         this.button7.Text = "5";
+         // 
+         // button8
+         // 
+         this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button8.Location = new System.Drawing.Point( 0, 80 );
+         this.button8.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button8.Name = "button8";
+         this.button8.Size = new System.Drawing.Size( 63, 50 );
+         this.button8.TabIndex = 22;
+         this.button8.Text = "4";
+         // 
+         // button4
+         // 
+         this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button4.Location = new System.Drawing.Point( 189, 30 );
+         this.button4.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button4.Name = "button4";
+         this.button4.Size = new System.Drawing.Size( 63, 50 );
+         this.button4.TabIndex = 21;
+         this.button4.Text = "/";
+         // 
+         // button3
+         // 
+         this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button3.Location = new System.Drawing.Point( 126, 30 );
+         this.button3.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button3.Name = "button3";
+         this.button3.Size = new System.Drawing.Size( 63, 50 );
+         this.button3.TabIndex = 20;
+         this.button3.Text = "9";
+         // 
+         // button2
+         // 
+         this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button2.Location = new System.Drawing.Point( 63, 30 );
+         this.button2.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button2.Name = "button2";
+         this.button2.Size = new System.Drawing.Size( 63, 50 );
+         this.button2.TabIndex = 19;
+         this.button2.Text = "8";
+         // 
+         // button1
+         // 
+         this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+         this.button1.Location = new System.Drawing.Point( 0, 30 );
+         this.button1.Margin = new System.Windows.Forms.Padding( 0 );
+         this.button1.Name = "button1";
+         this.button1.Size = new System.Drawing.Size( 63, 50 );
+         this.button1.TabIndex = 18;
+         this.button1.Text = "7";
+         // 
+         // textBox1
+         // 
+         this.textBox1.Location = new System.Drawing.Point( 0, 1 );
+         this.textBox1.Multiline = true;
+         this.textBox1.Name = "textBox1";
+         this.textBox1.Size = new System.Drawing.Size( 252, 26 );
+         this.textBox1.TabIndex = 17;
+         // 
+         // CalculatorGUIForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF( 7F, 15F );
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size( 252, 230 );
+         this.Controls.Add( this.button13 );
+         this.Controls.Add( this.button14 );
+         this.Controls.Add( this.button15 );
+         this.Controls.Add( this.button16 );
+         this.Controls.Add( this.button9 );
+         this.Controls.Add( this.button10 );
+         this.Controls.Add( this.button11 );
+         this.Controls.Add( this.button12 );
+         this.Controls.Add( this.button5 );
+         this.Controls.Add( this.button6 );
+         this.Controls.Add( this.button7 );
+         this.Controls.Add( this.button8 );
+         this.Controls.Add( this.button4 );
+         this.Controls.Add( this.button3 );
+         this.Controls.Add( this.button2 );
+         this.Controls.Add( this.button1 );
+         this.Controls.Add( this.textBox1 );
+         this.Font = new System.Drawing.Font( "Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ( ( byte ) ( 0 ) ) );
+         this.Name = "CalculatorGUIForm";
+         this.Text = "Calculator GUI";
+         this.ResumeLayout( false );
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button button13;
+      private System.Windows.Forms.Button button14;
+      private System.Windows.Forms.Button button15;
+      private System.Windows.Forms.Button button16;
+      private System.Windows.Forms.Button button9;
+      private System.Windows.Forms.Button button10;
+      private System.Windows.Forms.Button button11;
+      private System.Windows.Forms.Button button12;
+      private System.Windows.Forms.Button button5;
+      private System.Windows.Forms.Button button6;
+      private System.Windows.Forms.Button button7;
+      private System.Windows.Forms.Button button8;
+      private System.Windows.Forms.Button button4;
+      private System.Windows.Forms.Button button3;
+      private System.Windows.Forms.Button button2;
+      private System.Windows.Forms.Button button1;
+      private System.Windows.Forms.TextBox textBox1;
+   }
+}
+
---
+++ CalculatorGUI/CalculatorGUIForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalculatorGUI
+{
+   public partial class CalculatorGUIForm : Form
+   {
+      public CalculatorGUIForm()
+      {
+         InitializeComponent();
+      }
+   }
+}
+
+/**************************************************************************
+ * (C) Copyright 1992-2017 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
